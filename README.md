9ffdc01191fa8c76860e1a77265e13b2405381ea
cyberprofali <aliev.mordecai@gmail.com>
2026-02-14 11:29:23 -0600

    Adding sample test

---
+++ README.md
@@ -0,0 +1,3 @@
+# README FILE
+
+This is your read me file
---
+++ sample-portscanner.py
@@ -0,0 +1,16 @@
+import socket
+import sys
+from datetime import datetime
+
+def scan_port(target, port):
+    # TODO: Fill out this
+
+def validate_target(target):
+    #TODO: Fill out this
+
+def main():
+    #TODO: Research this please
+
+#TODO: Figure out what __name__ does and why this is important?
+if __name__=="__main__":
+    main()
